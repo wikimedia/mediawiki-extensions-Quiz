277ffe683089e162e20670a69cb6d7696d60c858
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-06-19 23:07:28 +0000

    Localisation updates for extension messages from translatewiki.net (2009-06-19 21:51 UTC)

--- Quiz.i18n.php
+++ Quiz.i18n.php
@@ -904,10 +904,11 @@ $messages['km'] = array(
 /** Korean (한국어)
  * @author Ilovesabbath
  * @author Kwj2772
+ * @author Yknok29
  */
 $messages['ko'] = array(
 	'quiz_desc' => '퀴즈 생성을 허용',
-	'quiz_addedPoints' => '$1점이 정답으로 추가되었습니다.',
+	'quiz_addedPoints' => '$1{{PLURAL:$1|점|점}}이 정답으로 추가되었습니다.',
 	'quiz_cutoffPoints' => '틀린 답마다 {{PLURAL:$1|Point|Points}} 감점',
 	'quiz_ignoreCoef' => '질문 계수 무시',
 	'quiz_shuffle' => '문제 섞기',
