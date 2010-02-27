ba45caec3dd2fea5429d511403151f3398bda52b
Raimond Spekking <raymond@users.mediawiki.org>
2010-02-27 19:16:22 +0000

    Localisation updates for core and extension messages from translatewiki.net (2010-02-27 19:00 UTC)

--- Quiz.i18n.php
+++ Quiz.i18n.php
@@ -1349,7 +1349,7 @@ $messages['ps'] = array(
 	'quiz_colorRight' => 'سم',
 	'quiz_colorWrong' => 'ناسم',
 	'quiz_colorNA' => 'بې ځوابه',
-	'quiz_score' => 'ستاسو نومرې $1 / $2 دي',
+	'quiz_score' => 'ستاسې نومرې $1 / $2 دي',
 	'quiz_points' => '$1 | {{PLURAL:$2|1 نمره|$2 نمرې}}',
 );
 
