ade1ba9f850ea111a26f76c6e4a765345ec2ae91
harjotsingh <harjot39@ymail.com>
2017-07-19 21:10:55 +0530

    Add tests for shuffle control tags {X}, {!X} and {/X}
    This change adds tests for shuffle control.The testParseQuestion function
    requires $input array for ParseQuestion, $expected value to assertEquals,
    injected value for mShuffleDiv and expected value for mShuffleDiv after
    output is returned from ParseQuesiton.Two assertions are made for each
    test case.
    
    Bug: T167635
    Change-Id: Ie2b66d949aacd3830e669c6b2d5ec4b0cee8fbcd

--- tests/phpunit/QuizTest.php
+++ tests/phpunit/QuizTest.php
@@ -9,7 +9,10 @@ class QuizTest extends MediaWikiLangTestCase {
 		parent::setUp();
 		global $wgParser;
 		$wgParser = $this->getParser();
+		$options = new ParserOptions();
+		$title = $wgParser->getTitle();
 		$this->parser = &$wgParser;
+		$this->parser->startExternalParse( $title, $options, 'text', true );
 		$this->quiz = new Quiz( [], $this->parser );
 	}
 
@@ -51,6 +54,47 @@ class QuizTest extends MediaWikiLangTestCase {
 	public function testResetQuizId() {
 		$this->quiz->resetQuizId();
 		$this->assertEquals( 0, $this->quiz->getQuizId() );
+	}
+
+	public function provideParseQuestion() {
+		return [
+			// Test for {X} tag
+			[
+			 [ [ 'X}', 'X', '' ], '1' ],
+			 '<div class="shuffle">' . "\n", '0', '1'
+			],
+			// Test for {!X} tag
+			[
+			 [ [ '!X}', '!X', '' ], '1' ],
+			 '<div class="noshuffle">' . "\n", '0', '1'
+			],
+			// Test for {/X} tag when {X} or {!X} has been used previously
+			[
+			 [ [ '/X}', '/X', '' ], '1' ],
+			 '', '0', '0'
+			],
+			// Test for {/X} tag when {X} or {!X} has not been used previously
+			[
+			 [ [ '/X}', '/X', '' ], '1' ],
+			 '</div>' . "\n", '1', '0'
+			],
+			// Test when random value is passed inside {} tag
+			[
+			 [ [ 'abc}', 'abc', '' ], '1' ],
+			 '<div class="quizText">abc<br /></div>' . "\n", '0', '0'
+			]
+		];
+	}
+
+	/**
+	 * @dataProvider provideParseQuestion
+	 * @covers Question::parseQuestion
+	 */
+	public function testParseQuestion( $input, $expected, $injectedValue, $expectedValue ) {
+		$this->quiz->mShuffleDiv = $injectedValue;
+		$output = $this->quiz->parseQuestion( $input[0], $input[1] );
+		$this->assertEquals( $this->quiz->mShuffleDiv, $expectedValue );
+		$this->assertEquals( $output, $expected );
 	}
 
 }
