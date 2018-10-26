bd18f932bf971ed281c4ffe90b48126334ff2fa6
Umherirrender <umherirrender_de.wp@web.de>
2018-10-26 21:28:14 +0200

    Start showing warning for deprecated PHP entry point
    Change-Id: I13c9c7b35a9524dcda9127bcc05bd3ac272c832e

--- Quiz.php
+++ Quiz.php
@@ -38,10 +38,10 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 if ( function_exists( 'wfLoadExtension' ) ) {
 	wfLoadExtension( 'Quiz' );
 	$wgMessagesDirs['QuizExtension'] = __DIR__ . '/i18n';
-	/* wfWarn(
+	wfWarn(
 		'Deprecated PHP entry point used for Quiz extension. Please use wfLoadExtension instead, ' .
 		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	); */
+	);
 	return;
 } else {
 	die( 'This version of the Quiz extension requires MediaWiki 1.25+' );
