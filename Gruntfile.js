97a763a41c0d88326f901f27eb3f0ba7b3390637
Umherirrender <umherirrender_de.wp@web.de>
2020-01-01 23:15:16 +0100

    build: Upgrading grunt-banana-checker to 0.8.1
    * Set requireLowerCase to 'initial' because some message keys have
      uppercase characters in them.
    
    Change-Id: I09d865aae0ab2163645ed6281e4a8e18864c540c

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,10 @@ module.exports = function ( grunt ) {
 
 	grunt.initConfig( {
 		banana: {
-			all: 'i18n/'
+			all: 'i18n/',
+			options: {
+				requireLowerCase: 'initial'
+			}
 		},
 		eslint: {
 			options: {
