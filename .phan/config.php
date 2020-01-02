df3f180ceee55042d7db5d82177e62b816b40048
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2020-01-02 08:26:25 +0000

    build: Updating composer dependencies
    * mediawiki/mediawiki-phan-config: 0.8.0 → 0.9.0
    * mediawiki/minus-x: 0.3.1 → 0.3.2
    
    Additional changes:
    * Added .eslintcache to .gitignore.
    
    Change-Id: I1d1347802c4c3c7590587d246752df00e6c31021

--- .phan/config.php
+++ .phan/config.php
@@ -1,3 +1,9 @@
 <?php
 
-return require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+$cfg = require __DIR__ . '/../vendor/mediawiki/mediawiki-phan-config/src/config.php';
+
+// These are too spammy for now. TODO enable
+$cfg['null_casts_as_any_type'] = true;
+$cfg['scalar_implicit_cast'] = true;
+
+return $cfg;
