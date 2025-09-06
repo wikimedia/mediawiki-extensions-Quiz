8c681e69c5b560ad31d36b625f4110fa0408cfba
Umherirrender <umherirrender_de.wp@web.de>
2025-09-06 23:17:02 +0200

    Use PHP8 constructor property promotion syntax for dependency injection
    Simplify the definition for injected services on the constructor.
    Also use readonly properties for services.
    
    Change-Id: I0830cd3c5abab56c564e14842651d329ef259725

--- includes/Hooks/HookRunner.php
+++ includes/Hooks/HookRunner.php
@@ -13,10 +13,7 @@ use MediaWiki\HookContainer\HookContainer;
 class HookRunner implements
 	QuizQuestionCreatedHook
 {
-	private HookContainer $hookContainer;
-
-	public function __construct( HookContainer $hookContainer ) {
-		$this->hookContainer = $hookContainer;
+	public function __construct( private readonly HookContainer $hookContainer ) {
 	}
 
 	/**
